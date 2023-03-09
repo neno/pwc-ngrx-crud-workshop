db52680e638b3f3745a943b5fa5c8a8c0132c2a6
Nenad Brcic <neno@neno.ch>
2023-03-09 13:30:03 +0100

    Add docs

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
+  { path: 'docs', loadChildren: () => import('./docs/docs.module').then(m => m.DocsModule) },
   { path: '', loadChildren: () => import('./events/events.module').then(m => m.EventsModule)},
 ];
 
